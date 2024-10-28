7733407451767ecc4f6a67f9bf87c85f8e71aa66
hbar1st <35541470+hbar1st@users.noreply.github.com>
2024-10-28 12:16:05 -0400

    invalid password highlighting

--- script.js
+++ script.js
@@ -2,17 +2,18 @@ const passwd = document.querySelector('#password');
 const confPwd = document.querySelector('#confirm-pwd');
 const errorMsg = document.querySelector('#error');
 const createBtn = document.querySelector('button');
-const passwdDiv = document.querySelectorAll("div.has( input[type='password'])");
 
 confPwd.addEventListener("change",() => {
     if (passwd.value !== confPwd.value) {
         errorMsg.style.opacity = "1";
         createBtn.classList.add('off');
-        passwdDiv.classList.add('invalid');
+        passwd.classList.add('invalid');
+        confPwd.classList.add('invalid');
     } else {
         errorMsg.style.opacity = "0";
         createBtn.classList.remove('off');
-        passwdDiv.classList.add('invalid');
+        passwd.classList.remove('invalid');
+        confPwd.classList.remove('invalid');
     }
     console.log(confPwd.value);
 });
